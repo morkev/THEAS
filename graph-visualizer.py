b5f0ba30fc870eafb01e407cc3c58f2bf5c0f036
Kevin Mora <sci.kevinmora@gmail.com>
2024-03-19 14:33:15 -0700

    added get-pip for users that may need it and changed the filename to match documentation

--- graph-visualizer.py
+++ graph-visualizer.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-filename = "resources/ahuna-mons.csv"
+filename = "datasets/celeste.csv"
 
 # Use NumPy arrays instead of Python lists
 latitude_list = np.array([])
